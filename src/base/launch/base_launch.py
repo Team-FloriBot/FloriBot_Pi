4797ebeb2045f09f9e2dbd9a0deb3cd043316993
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2025-12-25 15:42:56 +0100

    Update base_launch.py
    added encoder

--- src/base/launch/base_launch.py
+++ src/base/launch/base_launch.py
@@ -3,12 +3,19 @@ from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
+
 def generate_launch_description():
     pkg = get_package_share_directory('base')
     kin = os.path.join(pkg, 'params', 'kinematics_params.yaml')
     hw  = os.path.join(pkg, 'params', 'hardware_params.yaml')
 
     return LaunchDescription([
+        Node(
+            package='base',
+            executable='sensor_aggregator_node',
+            name='sensor_aggregator_node',
+            output='screen',
+        ),
         Node(
             package='base',
             executable='kinematics_node',
