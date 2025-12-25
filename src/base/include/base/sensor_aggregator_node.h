993acec1a3928f4f139a7ee74491e357246d1ff3
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2025-12-25 16:57:47 +0100

    Update sensor_aggregator_node.h

--- src/base/include/base/sensor_aggregator_node.h
+++ src/base/include/base/sensor_aggregator_node.h
@@ -3,8 +3,11 @@
 
 #include <cstdint>
 #include <cmath>
+#include <string>
 
 #include "rclcpp/rclcpp.hpp"
+#include "sensor_msgs/msg/joint_state.hpp"
+
 #include "base/msg/wheel_velocities.hpp"
 #include "base/msg/wheel_controller_state.hpp"
 #include "base/msg/wheel_ticks4.hpp"
@@ -22,17 +25,18 @@ private:
     void rlStateCallback(const base::msg::WheelControllerState::SharedPtr msg);
     void rrStateCallback(const base::msg::WheelControllerState::SharedPtr msg);
 
+    // NEW: JointStates von Phidget HighSpeedEncoder (liefert joint0..joint3)
+    void jointStatesCallback(const sensor_msgs::msg::JointState::SharedPtr msg);
+
     // Haupt-Timer-Funktion zur Aggregation und Veröffentlichung
     void publishAggregatedStates();
-
-    // Tick-Integration aus gemessener Winkelgeschwindigkeit (rad/s)
-    void updateTicks(double omega_rad_s, int64_t &tick_counter);
 
     // Abonnements
     rclcpp::Subscription<base::msg::WheelControllerState>::SharedPtr sub_fl_;
     rclcpp::Subscription<base::msg::WheelControllerState>::SharedPtr sub_fr_;
     rclcpp::Subscription<base::msg::WheelControllerState>::SharedPtr sub_rl_;
     rclcpp::Subscription<base::msg::WheelControllerState>::SharedPtr sub_rr_;
+    rclcpp::Subscription<sensor_msgs::msg::JointState>::SharedPtr sub_joint_states_;
 
     // Publisher
     rclcpp::Publisher<base::msg::WheelVelocities>::SharedPtr pub_wheel_states_;
@@ -41,18 +45,16 @@ private:
     // Gespeicherte Zustände (die letzten gemessenen Geschwindigkeiten)
     base::msg::WheelVelocities current_velocities_;
 
-    // Kumulative Encoderstände (integriert)
+    // Kumulative Encoderstände (aus /joint_states übernommen)
     int64_t fl_ticks_{0};
     int64_t fr_ticks_{0};
     int64_t rl_ticks_{0};
     int64_t rr_ticks_{0};
 
-    // Zeitbasis für Integration
-    rclcpp::Time last_update_time_;
-    bool time_initialized_{false};
-
-    // Encoderauflösung (Quadratur bereits enthalten; ggf. anpassen)
-    static constexpr int64_t TICKS_PER_REV = 2048;
+    // JointState Handling
+    std::string joint_states_topic_{"/joint_states"};
+    rclcpp::Time last_joint_stamp_;
+    bool have_joint_state_{false};
 
     // Timer
     rclcpp::TimerBase::SharedPtr timer_;
