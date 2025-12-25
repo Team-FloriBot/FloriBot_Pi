e82130b6faee059ed5df19c3b1b26a50a4b03cc9
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2025-12-25 17:26:46 +0100

    Update base_launch.py

--- src/base/launch/base_launch.py
+++ src/base/launch/base_launch.py
@@ -1,6 +1,7 @@
 from launch import LaunchDescription
-from launch_ros.actions import Node, IncludeLaunchDescription
+from launch.actions import IncludeLaunchDescription
 from launch.launch_description_sources import PythonLaunchDescriptionSource
+from launch_ros.actions import Node
 from ament_index_python.packages import get_package_share_directory
 import os
 
