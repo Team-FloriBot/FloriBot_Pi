b918cc3cf7e8db21bd877bd8657bce32f6fd5277
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-01-10 16:11:50 +0100

    Update base_launch.py

--- src/base/launch/base_launch.py
+++ src/base/launch/base_launch.py
@@ -4,26 +4,23 @@ from ament_index_python.packages import get_package_share_directory
 import os
 
 def generate_launch_description():
-    pkg_base = get_package_share_directory('base')
-    kin = os.path.join(pkg_base, 'params', 'kinematics_params.yaml')
-    hw  = os.path.join(pkg_base, 'params', 'hardware_params.yaml')
+    pkg_share = get_package_share_directory('base')
+    kin_params = os.path.join(pkg_share, 'params', 'kinematics_params.yaml')
+    hw_params  = os.path.join(pkg_share, 'params', 'hardware_params.yaml')
 
     return LaunchDescription([
-        # --- Hardware (Motor + Encoder -> /base/wheel_ticks4) ---
-        Node(
-            package='base',
-            executable='hardware_node',
-            name='hardware_node',
-            output='screen',
-            parameters=[hw],
-        ),
-
-        # --- Kinematics (PID + Odom, publishes /base/wheel_cmd) ---
         Node(
             package='base',
             executable='kinematics_node',
             name='kinematics_node',
             output='screen',
-            parameters=[kin],
+            parameters=[kin_params],
+        ),
+        Node(
+            package='base',
+            executable='hardware_node',
+            name='hardware_node',
+            output='screen',
+            parameters=[hw_params],
         ),
     ])
