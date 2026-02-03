718247aa8ae52cad9dd0274388d46d62adb2596f
TimoZimmermann <78686561+TimoZimmermann@users.noreply.github.com>
2026-02-03 12:12:33 +0100

    Update base_launch.py

--- src/base/launch/base_launch.py
+++ src/base/launch/base_launch.py
@@ -8,22 +8,11 @@ import os
 
 def generate_launch_description():
     base_share = get_package_share_directory("base")
-    phidgets_share = get_package_share_directory("phidgets_high_speed_encoder")
+   
 
     hardware_params = os.path.join(base_share, "params", "hardware_params.yaml")
     kinematics_params = os.path.join(base_share, "params", "kinematics_params.yaml")
 
-    phidgets_launch = os.path.join(phidgets_share, "launch", "high_speed_encoder-launch.py")
-
-    phidgets_node = IncludeLaunchDescription(
-        PythonLaunchDescriptionSource(phidgets_launch),
-        launch_arguments={
-            "joint0_tick2rad": "1.0",
-            "joint1_tick2rad": "1.0",
-            "joint2_tick2rad": "1.0",
-            "joint3_tick2rad": "1.0",
-        }.items()
-    )
 
     kinematics_kwargs = {
         "package": "base",
@@ -34,8 +23,7 @@ def generate_launch_description():
     }
 
     return LaunchDescription([
-        phidgets_node,
-
+        
         Node(
             package="base",
             executable="hardware_node",
